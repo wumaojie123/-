d7de790905ab1630f8a44e52bcc062d40671f859
licenlc <licenlc@163.com>
2018-10-14 01:28:00 +0800

    代理商列表接口

--- src/api/angentManage.js
+++ src/api/angentManage.js
@@ -2,24 +2,40 @@ import request from '@/utils/request'
 
 export function fetchList(query) {
   return request({
-    url: '/transaction/list',
+    url: '/agentManage/queryAgents',
     method: 'get',
     params: query
   })
 }
 
-export function add(query) {
+export function getCode(query) {
   return request({
-    url: '/transaction/list',
-    method: 'post',
-    query
+    url: '/agentManage/sendCode',
+    method: 'get',
+    params: query
   })
 }
 
-export function update(query) {
+export function update(data) {
   return request({
-    url: '/transaction/list',
+    url: '/agentManage/addOrUpdateAgent',
     method: 'post',
-    query
+    data
+  })
+}
+
+export function getAngent(query) {
+  return request({
+    url: '/agentManage/getAgent',
+    method: 'get',
+    params: query
+  })
+}
+
+export function getMerchant(query) {
+  return request({
+    url: '/agentManage/getMerchant',
+    method: 'get',
+    params: query
   })
 }
