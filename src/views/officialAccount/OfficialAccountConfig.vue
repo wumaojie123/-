f04b194ee81fb87f6f46bb20d95ce45549d2e22e
liuss <liushanshan@leyaoyao.com>
2019-09-10 19:39:14 +0800

    公众号

--- src/views/officialAccount/OfficialAccountConfig.vue
+++ src/views/officialAccount/OfficialAccountConfig.vue
@@ -2,9 +2,19 @@
   <div class="content-area">
     <!-- 列表 -->
     <el-table :data="list" border highlight-current-row style="width: 100%;margin-bottom: 20px;">
-      <el-table-column v-for="(item, index) in colums" :key="index" :prop="item.key" :label="item.label" :width="item.width" :sortable="item.sortable" align="center"/>
-      <el-table-column label="操作" align="center" >
+      <el-table-column
+        v-for="(item, index) in colums"
+        :key="index"
+        :prop="item.key"
+        :label="item.label"
+        :width="item.width"
+        :sortable="item.sortable"
+        align="center"
+      />
+      <el-table-column label="操作" align="center">
         <template slot-scope="scope">
+          <el-button type="text" @click="unbind(scope.row)">公众号解绑</el-button>
+          <el-button type="text" @click="resetValidDate(scope.row)">重置授权有效期</el-button>
           <el-button type="text" @click="handleNewAuth(scope.row)">重新授权</el-button>
           <el-button type="text" @click="handleConfig(scope.row)">公众号功能配置</el-button>
         </template>
@@ -18,33 +28,65 @@
       background
       layout="total, prev, pager, next, sizes, jumper"
       @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"/>
+      @current-change="handleCurrentChange"
+    />
+    <el-dialog :visible.sync="dialogVisible" title="重置授权有效期" center>
+      <re-authorize :day="vilidDay" />
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { getAllResult } from '@/api/officialAccount'
+import reAuthorize from './component/reAuthorize'
 
 export default {
+  components: { reAuthorize },
   data() {
     return {
       list: [],
       colums: [
         { key: 'officialAccountName', label: '公众号名称' },
-        { key: 'equipmentTypeValue', label: '绑定设备类型' }
+        { key: 'equipmentTypeValue', label: '绑定设备类型' },
+        // modify by lss 20190910
+        { key: 'day', label: '公众号授权剩余有效期（天）' }
       ],
       pageInfo: { total: 0, pageSize: 10, currPage: 1 },
-      selectList: []
+      selectList: [],
+      // modify by lss 20190910 start
+      dialogVisible: true,
+      vilidDay: '' // -1 不显示 0 过期  >0 未过期
+      // end
     }
   },
   created() {
     this.queryList()
   },
   methods: {
+    /**
+     * modify by lss 20190910
+     * 解绑
+     */
+    unbind(item) {
+      var title = `公众号${
+        item.officialAccountName
+      }确认解绑吗？<p style="color:red;font-size:0.24rem">解绑后沉淀粉丝、服务消息通知、配置菜单等功能将失效</p>`
+      this.$messagebox
+        .confirm(title, '公众号解绑')
+        .then(action => {})
+        .catch(() => {})
+    },
+    /**
+     * 重置有效期
+     */
+    resetValidDate(item) {},
     async queryList(page = 1) {
       this.list = []
       this.pageInfo.currPage = page
-      const postData = { pageSize: this.pageInfo.pageSize, pageIndex: this.pageInfo.currPage }
+      const postData = {
+        pageSize: this.pageInfo.pageSize,
+        pageIndex: this.pageInfo.currPage
+      }
       const res = await getAllResult(postData)
       if (res.result === 0) {
         this.listLoading = false
@@ -58,7 +100,9 @@ export default {
       }
     },
     handleNewAuth(item) {
-      window.location.href = `/agent/rest/officialAccounts/againAuthUrl?eType=${item.equipmentType}`
+      window.location.href = `/agent/rest/officialAccounts/againAuthUrl?eType=${
+        item.equipmentType
+      }`
     },
     handleConfig(item) {
       this.$router.push({
---
+++ src/views/officialAccount/component/reAuthorize.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="anthorize-wrap">
+    <div class="header">{{ title }}</div>
+    <div class="content-wrap">
+      <h3>如何重置授权有效期？</h3>
+      <div>
+        <div class="img-wrap">
+          <img src="../images/authorize-bg.png" >
+        </div>如何重置授权有效期？点击底部【重置授权有效期】按钮，进入扫码授权页，用公众号管理员手机微信扫描二维码进入权限确认页面，若【素材管理】权限处于已勾选状态，则去掉勾选后点击授权；若【素材管理】权限处于非勾选状态，则勾选后点击授权；
+      </div>
+    </div>
+    <div class="footer" @click="reAuthorize">重置授权有效期</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    day: {
+      type: [Number, String],
+      default: ''
+    }
+  },
+  computed: {
+    title() {
+      if (this.day === -1) {
+        return '公众号失效时，【重置授权有效期】即可恢复公众号功能。'
+      }
+      if (this.day > 0) {
+        return `您的公众号授权剩余有效期：${
+          this.day
+        }天，到期前请【重置授权有效期】。`
+      }
+      return '您的公众号授权已过期，【重置授权有效期】即可恢复公众号功能'
+    }
+  },
+  created() {
+    console.log(this.$route.params.id)
+  },
+  methods: {
+    reAuthorize() {}
+  }
+}
+</script>
+<style lang="less" scoped>
+.anthorize-wrap {
+  //   position: absolute;
+  //   top: 0;
+  //   left: 0;
+  //   bottom: 0;
+  //   right: 0;
+  //   overflow: scroll;
+  .content-wrap {
+    padding: 26px 16px;
+    background: #fff;
+  }
+  .header {
+    width: 100%;
+    background: rgba(237, 244, 255, 1);
+    height: 36px;
+    line-height: 36px;
+    color: rgba(61, 123, 232, 1);
+    text-align: center;
+  }
+  .img-wrap {
+    width: 100%;
+    height: 500px;
+  }
+  .img-wrap > img {
+    width: 100%;
+    height: 100%;
+  }
+  .footer {
+    // position: fixed;
+    // bottom: 0;
+    height: 80px;
+    line-height: 80px;
+    background: rgba(61, 123, 232, 1);
+    width: 100%;
+    color: rgba(255, 255, 255, 1);
+    text-align: center;
+  }
+}
+</style>
