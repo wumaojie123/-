9db04e58cfc6e217662892343acfe511cfb48bee
liuss <liushanshan@leyaoyao.com>
2019-09-11 17:28:49 +0800

    优化

--- src/views/officialAccount/OfficialAccountConfig.vue
+++ src/views/officialAccount/OfficialAccountConfig.vue
@@ -11,7 +11,7 @@
         :sortable="item.sortable"
         align="center"
       />
-      <el-table-column label="操作" align="center">
+      <el-table-column label="操作" align="center" width="400">
         <template slot-scope="scope">
           <el-button type="text" @click="unbind(scope.row)">公众号解绑</el-button>
           <el-button type="text" @click="resetValidDate(scope.row)">重置授权有效期</el-button>
